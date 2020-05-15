39c2a5db580875221acf1c1f91e334ca5cd94f7e
Alex Gubskyy <aleks.gubskyy@gmail.com>
2020-05-15 17:22:56 +0200

    init: prod

---
+++ precache-manifest.dd39b7725b474baabae4e3fcc9502897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0cef65f2f24da49b1a85b616055897",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5747c17b33583823e28",
+    "url": "/static/css/main.2dbd39ee.chunk.css"
+  },
+  {
+    "revision": "8bbb9d19ce311c948a86",
+    "url": "/static/js/2.f82018e6.chunk.js"
+  },
+  {
+    "revision": "e5747c17b33583823e28",
+    "url": "/static/js/main.57e3fd90.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
